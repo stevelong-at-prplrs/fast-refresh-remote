350d1146eb9eee498f0822c611a189615880eb8c
Steve Long <stevelong@prpl.rs>
2023-05-14 19:50:32 -0500

    add a comment

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./app";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app")); // this is not the react 18 way of doing this
